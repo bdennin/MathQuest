87a1fe0b493d86ee84e58f942d459049b900f72f
zhiliXu <zx0804@westminstercollege.edu>
2015-02-04 16:52:09 -0700

    gh

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -2,6 +2,9 @@ import javax.swing.JPanel;
 
 public class GameWorld extends JPanel 
 {
-	ÄÇÃ´
-·¶µÂÈø
+ public static void main(String arg[]){
+	 System.out.println("1");
+	 System.out.println("2");
+	 
+ }
 }
