cdffdba0bcc77179240fb284acea330926599b55
kbroten14 <kmbroten14@gmail.com>
2015-02-17 18:51:12 -0700

    more character, monster, combat
    added toString to character, constructor in combat to make specific
    leveled monsters.

--- src/Combat.java
+++ src/Combat.java
@@ -4,39 +4,87 @@ public class Combat {
 	private static Character playerCharacter;
 	private static Monster enemyMonster;
 	private boolean isPlayerTurn;
+	private Scanner input;
+	private String playerChoice;
 	
-	public Combat(Character c, Monster m , boolean turn){
-		isPlayerTurn = turn;
+	
+	public Combat(Character c, Monster m){
 		playerCharacter = c;
 		enemyMonster = m;
+		input = new Scanner(System.in);
 	}
 	
-	private boolean playerTurn(Character c){
-		System.out.println("Player's Turn");
-		System.out.println("Health: " + c.getCurrentHealth() + " / " + c.getMaxHealth());
-		return isPlayerTurn = false;
-	}
-	
-	private boolean monsterTurn(Monster m){
-		System.out.println("Monster's Turn");
-		return isPlayerTurn = true;
-	}
-	
-	private int fight(Character c, Monster m){
-		Scanner input = new Scanner(System.in);
+	public Combat(Character c, int l){
+		playerCharacter = c;
+		input = new Scanner(System.in);
+		if(l > 2 && l < 4){
+			enemyMonster = new Monster(30, 300, 3, 30, 30, 30);
+		}
+		else if(l == 2){
+			enemyMonster = new Monster(20, 200, 2, 20, 20, 20);
+		}
+		else{
+			enemyMonster = new Monster();
+		}
 		
+	}
+	
+	
+//modify character and monster stats for level difference
+	private void sizeUp(){
+		if(playerCharacter.getLevel() - enemyMonster.getLevel() > 5){
+			enemyMonster.setExperience(enemyMonster.getExperience() * .25);
+			//other modifiers for higher player level than monster level
+		}
+		
+	}
+	
+	private String playerTurn(){
+		while(isPlayerTurn && enemyMonster.getCurrentHealth() > 0){
+			System.out.println("Attack or potion");
+			//does math equation for character's attack or potion
+			playerChoice = input.nextLine();
+			if(playerChoice.equalsIgnoreCase("attack")){
+				//character deals damage
+			}
+			if(playerChoice.equalsIgnoreCase("potion")){
+				//character drinks potion
+			}
+			isPlayerTurn = false;
+		}
+		
+		return "End of Turn";
+	}
+	
+	private String monsterTurn(){
+		while(!isPlayerTurn && playerCharacter.getCurrentHealth() > 0){
+			System.out.println("Monster is attacking");
+			//does math equation for monster attack that character can defend
+			
+			isPlayerTurn = true;
+		}
+		
+		return "Your Turn";
+	}
+	
+	private int fight(){
+		while(playerCharacter.getCurrentHealth() != 0 && enemyMonster.getCurrentHealth() != 0){
+			
+			System.out.println(this.playerTurn());
+			System.out.println(enemyMonster.takeDamage(playerCharacter.getStrength()));
+			System.out.println(this.monsterTurn());
+		}
 		
 		return 0;
 	}
 	
 	
-//	public static void main(String[] args){
-//		Character me = new Character();
-//		Monster you = new Monster();
-//		
-//		
-//		Combat testFight = new Combat(me,you,true);
-//		
-//		testFight.fight();
-//	}
+	public static void main(String[] args){
+		Character player = new Character();
+		Monster enemy = new Monster();
+		
+		Combat testFight = new Combat(player, enemy);
+		
+		testFight.fight();
+	}
 }
--- src/Monster.java
+++ src/Monster.java
@@ -1,37 +1,76 @@
 
 public class Monster {
 	
-	private int damage;
-	private int health;
+	private double damage;
+	private int currentHealth, maxHealth;
 	private int level;
-	private int gold;
-	private int experience;
+	private double gold;
+	private double experience;
 	private int armor;
+	private int speed;
+	private String name;
 	
 	public Monster(){
-		damage = 0;
-		health = 10;
+		name = "Monster";
+		damage = 10;
+		currentHealth = maxHealth = 100;
 		level = 1;
 		gold = 10;
 		experience = 10;
 		armor = 0;
+		speed = 10;
 	}
 	
-	public Monster(int lev){
-		damage = damage * lev;
-		health = health * lev;
-		level = lev;
-		gold = gold * lev;
-		experience = experience * lev;
-		armor = armor + lev;
-	}
 	
-	public Monster(int damage, int health, int level, int gold, int experience, int armor) {
+	//constructor for a monster that will be scaled to a specific level
+	
+	public Monster(int damage, int maxHealth, int level, int gold, int experience, int armor) {
 		this.damage = damage;
-		this.health = health;
+		this.maxHealth = maxHealth;
+		currentHealth = maxHealth;
 		this.level = level;
 		this.gold = gold;
 		this.experience = experience;
 		this.armor = armor;
 	}
+	
+	
+	public int getMaxHealth(){
+		return maxHealth;
+	}
+	
+	public int getCurrentHealth(){
+		return currentHealth;
+	}
+	
+	public int getLevel(){
+		return level;
+	}
+	
+	public int getArmor(){
+		return armor;
+	}
+	
+	public double getGold(){
+		return gold;
+	}
+	
+	public void setGold(int g){
+		gold = g;
+	}
+	
+	public double getExperience(){
+		return experience;
+	}
+	
+	public void setExperience(double e){
+		experience = e;
+	}
+	
+	//takes the strength of the character 
+	public int takeDamage(int s){
+		return currentHealth = currentHealth - s;
+	}
+	
+	
 }
