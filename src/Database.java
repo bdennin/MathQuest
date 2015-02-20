5ec5508fab151d327bc9b2a2957c390cfb2ad190
zhiliXu <zx0804@westminstercollege.edu>
2015-02-19 22:30:13 -0700

    add a method which can create an account

--- src/Database.java
+++ src/Database.java
@@ -2,10 +2,14 @@ import java.io.*;
 import java.util.*;
 import java.sql.*;
 
-public class Login
+public class Database
 {
- private static String name = null;
+ private static int userID;
  private static Connection con = null;
+ 
+ /**
+  * @return true if program connects Cerberus successfully
+  */
  public static boolean getConnected(){
   try{
    String username = "zxu";
@@ -25,16 +29,45 @@ public class Login
   }
  }
 
+ /**
+  * 
+  * @param inputName
+  * @param inputPass
+  * @param inputType
+  * @return true if save the information successfully. Otherwise, the input username already exists in the database.
+  */
+ public static boolean createAccount(String inputName, String inputPass, String inputType){
+	 try{
+		  Statement select = con.createStatement();
+		  int res = select.executeUpdate("INSERT IGNORE INTO Login (username, password, type) VALUES (\"" + inputName + "\" ,\"" + inputPass + "\" ,\"" + inputType + "\")" );
+		  if (res == 1){
+			con.close();
+		    return true;
+		  }
+		   con.close();
+		   return false;
+		  }
+		  catch (SQLException e){
+		   System.out.println("Error from createAccount: " + e.getMessage());
+		   return false;
+		  }
+ }
+ /**
+  * return true if given username and password match with database
+  * return false if either username is invalid or password is invalid
+  */
  public static boolean isValid(String inputName, String inputPass){
-  name = inputName;
   try{
   Statement select = con.createStatement();
-  ResultSet res = select.executeQuery("SELECT password from login where username = \"" + name + "\"") ;
+  ResultSet res = select.executeQuery("SELECT password, userID from Login where username = \"" + inputName + "\"") ;
   String storedPass =  null;
   if(res.next()){
-   storedPass= res.getString(1);
-     if(storedPass.equals(inputPass))
-     return true;
+	  storedPass= res.getString("password");
+     if(storedPass.equals(inputPass)){
+    	userID = res.getInt("userID");
+    	System.out.println(userID);
+    	 return true;
+     }
   }
    con.close();
    return false;
@@ -45,13 +78,16 @@ public class Login
   }
  }
  
+ /**
+  * @return the type of account
+  */
  public static String getType(){
   try{
   Statement select = con.createStatement();
-  ResultSet res = select.executeQuery("SELECT type from login where username = \"" + name + "\"");
+  ResultSet res = select.executeQuery("SELECT type from Login where userID = " + userID);
   String type = null;
   if(res.next())
-   type = res.getString(1);
+   type = res.getString("type");
   con.close();
   return type;
   }
@@ -60,4 +96,64 @@ public class Login
    return null;
   }
  }
+ 
+ /**
+  * 
+  * @return in order of level, currentHealth, exp and gold
+  */
+ public static Integer[] getStatus(){
+	 try{
+		  Statement select = con.createStatement();
+		  ResultSet res = select.executeQuery("SELECT * from Status where Login_userID =" + userID );
+		  Integer[] status = new Integer[4];
+		  if(res.next()){
+			  for(int index = 1;index < 5; index++)
+			   status[index-1] = res.getInt(index);
+		  }
+		  con.close();
+		  return status;
+		  }
+		  catch (SQLException e){
+		   System.out.println("Error from getStatus: " + e.getMessage());
+		   return null;
+		  }
+ }
+ 
+ /**
+  * 
+  * @param input string array should in order of level, currentHealth, exp and gold
+  * @return true if save the status successfully.
+  */
+ public static boolean setStatus(Integer[] status){
+	 try{
+		  PreparedStatement select = con.prepareStatement("UPDATE Status SET level = ?, currentHealth = ?, exp = ?, gold = ? where Login_userID = " + userID);
+		  int index = 1;
+		  for(Integer item : status){
+			  select.setInt(index, item);
+			  index++;
+		  }
+			  int res = select.executeUpdate();
+		  if (res == 1){
+		  con.close();
+		  return true; 
+		  }
+		  else
+			  con.close();
+		  return false;
+		  }
+		  catch (SQLException e){
+		   System.out.println("Error from setStatus: " + e.getMessage());
+		   return false;
+		  }
+ }
+ /*
+ public static void main(String[] args){
+	 Database.getConnected();
+	 //Database.userID = 1;
+	 //Database.getStatus();
+	 //Integer [] status = {2,30,2,30};
+	 //System.out.println(Database.setStatus(status));
+	 System.out.println(Database.createAccount("zxu123","1234","student"));
+ }
+ */
 }
