b48008538e4e752c03757d6220f219b83f20c338
JSchooley <brasilianschooley@hotmail.com>
2015-03-04 16:31:34 -0700

    Start of Loot

---
+++ src/MathQuest/Logic/Loot.java
@@ -0,0 +1,41 @@
+package MathQuest.Logic;
+
+import MathQuest.Inventory;
+
+public class Loot {
+
+	String itemName;
+	int itemLevel;
+	int dmg;
+	int str;
+	int dex;
+	int armor;
+	int gold;
+	int speed;
+	
+	public Loot(){
+		
+		itemName = "Terrible Item";
+		itemLevel = 1;
+		dmg = 0;
+		str = 0;
+		dex = 0;
+		armor = 0;
+		gold = 0;
+		speed = 0;
+		
+	}
+	
+	//Weapons
+	
+	//Armor-helmet
+	
+	//Armor-chest
+	
+	//Armor-legs
+	
+	//Armor-boots
+	
+	
+	
+}
