838ff36f300674fc09fa23fc00e6cfecf6a6ff4b
kbroten14 <kmbroten14@gmail.com>
2015-04-08 17:22:05 -0600

    Conformed like a sellout
    blacksmith menus now look like menus in other areas.

--- src/MathQuest/Pages/Blacksmith.java
+++ src/MathQuest/Pages/Blacksmith.java
@@ -11,6 +11,7 @@ import javax.swing.JTextArea;
 import MathQuest.GUI.OptionsPanel;
 import MathQuest.Logic.Character;
 import MathQuest.Logic.Item;
+import MathQuest.GUI.LogPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,23 +26,25 @@ import java.awt.Font;
 import javax.swing.border.BevelBorder;
 import javax.swing.JComboBox;
 import java.awt.Color;
+import javax.swing.Icon;
 
 public class Blacksmith extends Area {
 	
 	private static final long serialVersionUID = 1L;
-	private JTextArea scrollText;
+	private LogPanel scrollPane;
 	final JPanel buttonPanel = new JPanel();
-	final JPanel buyPanel, sellPanel, enhancePanel, armorPanel, weaponPanel;
+	final JPanel buyPanel, salePanel, sellPanel, enhancePanel, armorPanel, weaponPanel;
+	private ImageIcon buyButtonIcon, sellButtonIcon, enhanceButtonIcon;
 	
-	JComboBox inventoryComboBox, enhanceComboBox;
+	private JComboBox inventoryComboBox, enhanceComboBox;
 	
 	private Item item1, item2;
 	
 	private static final Character character = new Character();
 	
 	public Blacksmith(final Character hero) {
-		super(hero, "blacksmithMusic.mp3");
-//		super(character, null);
+//		super(hero, "blacksmithMusic.mp3");
+		super(character, null);
 		this.loadImages();
 		this.loadOptionsPanel();
 		
@@ -66,40 +69,25 @@ public class Blacksmith extends Area {
 		
 		
 		buttonPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		buttonPanel.setBounds(516, 599, 356, 138);
+		buttonPanel.setBounds(587, 612, 269, 77);
 		add(buttonPanel);
 		buttonPanel.setLayout(null);
-									
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(6, 6, 107, 126);
-		buttonPanel.add(panel_1);
-		panel_1.setLayout(null);
-												
-		JButton btnBuyItems = new JButton("Buy Items");
-		btnBuyItems.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(buyPanel.isVisible()){
-					scrollText.append("Ok\n");
-					buyPanel.setVisible(false);	
-				}
-				else{
-					scrollText.append("I think you could handle these.\n");
-					buyPanel.setVisible(true);
-				}
-			}
-		});
-		btnBuyItems.setBounds(0, 0, 101, 120);
-		panel_1.add(btnBuyItems);
-															
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(244, 6, 107, 126);
-		buttonPanel.add(panel_2);
-		panel_2.setLayout(null);
-																		
-		JButton btnImproveItems = new JButton("Improve Items");
+		
+		JButton btnBuyItems = new JButton(this.buyButtonIcon);
+		btnBuyItems.setBounds(3, 3, 88, 70);
+		buttonPanel.add(btnBuyItems);
+		
+		
+		JButton btnSellItems = new JButton(this.sellButtonIcon);
+		btnSellItems.setBounds(90, 3, 88, 70);
+		buttonPanel.add(btnSellItems);
+		
+		JButton btnImproveItems = new JButton(this.enhanceButtonIcon);
+		btnImproveItems.setBounds(177, 3, 88, 70);
+		buttonPanel.add(btnImproveItems);
 		btnImproveItems.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				scrollText.append("I can make anything stronger.\n");
+				scrollPane.addTextToScrollPane("I can make anything stronger.\n");
 				if(enhancePanel.isVisible()){
 					enhancePanel.setVisible(false);
 				}
@@ -108,70 +96,141 @@ public class Blacksmith extends Area {
 				}
 			}
 		});
-		btnImproveItems.setBounds(0, 0, 106, 120);
-		panel_2.add(btnImproveItems);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(125, 6, 107, 126);
-		buttonPanel.add(panel_3);
-		panel_3.setLayout(null);
 		
 		
-		JButton btnSellItems = new JButton("Sell Items");
 		btnSellItems.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(sellPanel.isVisible()){
-					scrollText.append("Ok.\n");
+					scrollPane.addTextToScrollPane("Ok.\n");
 					sellPanel.setVisible(false);
 				}
 				else{
-					scrollText.append("Let's see what you have.\n");
+					scrollPane.addTextToScrollPane("Let's see what you have.\n");
 					sellPanel.setVisible(true);
 				}
 			}
 		});
 		
-		btnSellItems.setBounds(0, 0, 101, 120);
-		panel_3.add(btnSellItems);
-																											
+		
+		btnBuyItems.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(salePanel.isVisible()){
+					scrollPane.addTextToScrollPane("Ok\n");
+					salePanel.setVisible(false);	
+				}
+				else{
+					scrollPane.addTextToScrollPane("I think you could handle these.\n");
+					salePanel.setVisible(true);
+				}
+			}
+		});
+		
+		scrollPane = new LogPanel("Blacksmith Log");
+		add(scrollPane);
+		this.scrollPane.addTextToScrollPane("Hello! I am the town Blacksmith. How can I help you?\n");
+		
+		sellPanel = new JPanel();
+		sellPanel.setBackground(Color.LIGHT_GRAY);
+		sellPanel.setBounds(735, 260, 245, 116);
+		sellPanel.setVisible(false);
+		add(sellPanel);
+		sellPanel.setLayout(null);
+		for(Item el : hero.getInventory())
+			if(!el.isEquipped()){
+				inventoryComboBox.addItem(el);
+			}
+		
+		JPanel sellButtonPanel = new JPanel();
+		sellButtonPanel.setBounds(1, 29, 244, 87);
+		sellPanel.add(sellButtonPanel);
+		sellButtonPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
+		sellButtonPanel.setLayout(null);
+		
+		inventoryComboBox = new JComboBox<Item>();
+		inventoryComboBox.setBounds(6, 6, 233, 27);
+		sellButtonPanel.add(inventoryComboBox);
+		
+		JButton sellBtn = new JButton("Sell");
+		sellBtn.setBounds(56, 45, 117, 35);
+		sellButtonPanel.add(sellBtn);
+		
+		JLabel sellLabel = new JLabel("Sell Items");
+		sellLabel.setBackground(Color.LIGHT_GRAY);
+		sellLabel.setBounds(1, 0, 244, 29);
+		sellPanel.add(sellLabel);
+		sellLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
+		sellLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		sellBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Item i = (Item)inventoryComboBox.getSelectedItem();
+				hero.addGold(i.getItemGold());
+				hero.removeFromInventory((Item)inventoryComboBox.getSelectedItem());
+				inventoryComboBox.removeItem(inventoryComboBox.getSelectedItem());
+				reloadInventoryPanel(false);
+			}
+		});
+		
+		enhancePanel = new JPanel();
+		enhancePanel.setBackground(Color.LIGHT_GRAY);
+		enhancePanel.setBounds(735, 422, 245, 105);
+		enhancePanel.setVisible(false);
+		add(enhancePanel);
+		enhancePanel.setLayout(null);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
+		panel_4.setBounds(0, 24, 245, 80);
+		enhancePanel.add(panel_4);
+		panel_4.setLayout(null);
+		
+		enhanceComboBox = new JComboBox();
+		enhanceComboBox.setBounds(6, 6, 233, 27);
+		panel_4.add(enhanceComboBox);
+		
+		JButton enhancheBtn = new JButton("Improve");
+		enhancheBtn.setBounds(51, 39, 117, 35);
+		panel_4.add(enhancheBtn);
+		
+		JLabel lblNewLabel_2 = new JLabel("Improve Items");
+		lblNewLabel_2.setBounds(0, 0, 245, 25);
+		enhancePanel.add(lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		for(Item el : hero.getInventory())
+			enhanceComboBox.addItem(el);
+		
+		salePanel = new JPanel();
+		salePanel.setBackground(Color.LIGHT_GRAY);
+		salePanel.setBounds(206, 225, 457, 248);
+		salePanel.setVisible(false);
+		add(salePanel);
+		salePanel.setLayout(null);
+		
 		buyPanel = new JPanel();
+		buyPanel.setBounds(0, 36, 456, 212);
+		salePanel.add(buyPanel);
 		buyPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		buyPanel.setBounds(267, 260, 456, 312);
-		buyPanel.setVisible(false);
-		add(buyPanel);
 		buyPanel.setLayout(null);
-		
-		JPanel armorLabelPanel = new JPanel();
-		armorLabelPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		armorLabelPanel.setBounds(19, 19, 417, 52);
-		buyPanel.add(armorLabelPanel);
-		armorLabelPanel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Items for Sale");
-		lblNewLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 23));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(6, 6, 405, 40);
-		armorLabelPanel.add(lblNewLabel);
 		
 		armorPanel = new JPanel();
 		armorPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		armorPanel.setBounds(19, 83, 198, 198);
+		armorPanel.setBounds(6, 6, 198, 198);
 		buyPanel.add(armorPanel);
 		armorPanel.setLayout(null);
 		
 		JLabel armorItemLabel = new JLabel(item1.toString());
+		armorItemLabel.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 15));
+		armorItemLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		armorItemLabel.setBounds(6, 6, 186, 25);
+		armorPanel.add(armorItemLabel);
 		if(item1.getColor().equalsIgnoreCase("gray")){
-			armorItemLabel.setForeground(Color.gray);
+			armorItemLabel.setForeground(Color.black);
 		}
 		else if(item1.getColor().equalsIgnoreCase("green")){
 			armorItemLabel.setForeground(Color.green);
 		}
 		else 
 			armorItemLabel.setForeground(Color.blue);
-		armorItemLabel.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 15));
-		armorItemLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		armorItemLabel.setBounds(6, 6, 186, 25);
-		armorPanel.add(armorItemLabel);
 		
 		JLabel lblVit = new JLabel("Vitality:");
 		lblVit.setBounds(6, 135, 61, 16);
@@ -180,7 +239,6 @@ public class Blacksmith extends Area {
 		JLabel lblStrength = new JLabel("Strength:");
 		lblStrength.setBounds(6, 112, 61, 16);
 		armorPanel.add(lblStrength);
-		
 		
 		JLabel strNumber = new JLabel("" + item1.getItemStr());
 		strNumber.setBounds(131, 112, 61, 16);
@@ -201,7 +259,7 @@ public class Blacksmith extends Area {
 					inventoryComboBox.addItem(item1);
 				}
 				else{
-					scrollText.append("You don't have enough for that\n");
+					scrollPane.addTextToScrollPane("You don't have enough for that\n");
 				}
 			}
 		});
@@ -215,23 +273,23 @@ public class Blacksmith extends Area {
 		weaponPanel = new JPanel();
 		weaponPanel.setLayout(null);
 		weaponPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		weaponPanel.setBounds(238, 83, 198, 198);
+		weaponPanel.setBounds(252, 6, 198, 198);
 		buyPanel.add(weaponPanel);
 		
 		JLabel weaponItemLabel = new JLabel(item2.toString());
-		if(item2.getColor().equalsIgnoreCase("gray")){
-			weaponItemLabel.setForeground(Color.GRAY);
-		}
-		else if(item2.getColor().equalsIgnoreCase("green")){
-			weaponItemLabel.setForeground(Color.GREEN);
-		}
-		else{
-			weaponItemLabel.setForeground(Color.BLUE);
-		}
 		weaponItemLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		weaponItemLabel.setFont(new Font("Copperplate Gothic Light", Font.PLAIN, 15));
 		weaponItemLabel.setBounds(6, 6, 186, 25);
 		weaponPanel.add(weaponItemLabel);
+		if(item2.getColor().equalsIgnoreCase("gray")){
+			weaponItemLabel.setForeground(Color.black);
+		}
+		else if(item2.getColor().equalsIgnoreCase("green")){
+			weaponItemLabel.setForeground(Color.green);
+		}
+		else{
+			weaponItemLabel.setForeground(Color.green);
+		}
 		
 		JLabel lblWeaponStrength = new JLabel("Strength:");
 		lblWeaponStrength.setBounds(6, 113, 61, 16);
@@ -260,7 +318,7 @@ public class Blacksmith extends Area {
 					inventoryComboBox.addItem(item2);
 				}
 				else{
-					scrollText.append("You don't have enough for that\n");
+					scrollPane.addTextToScrollPane("You don't have enough for that\n");
 				}
 			}
 		});
@@ -271,91 +329,12 @@ public class Blacksmith extends Area {
 		weaponVitLabel.setBounds(131, 133, 61, 16);
 		weaponPanel.add(weaponVitLabel);
 		
-		JPanel textPanel = new JPanel();
-		textPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		textPanel.setBounds(166, 599, 338, 138);
-		add(textPanel);
-		textPanel.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 6, 326, 128);
-		textPanel.add(scrollPane);
-		
-		this.scrollText = new JTextArea();
-		this.scrollText.setEditable(false);
-		this.scrollText.setLineWrap(true);
-		this.scrollText.setWrapStyleWord(true);
-		scrollPane.setColumnHeaderView(scrollText);
-		this.scrollText.append("Hello! I am the town Blacksmith. How can I help you?\n");
-		
-		sellPanel = new JPanel();
-		sellPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		sellPanel.setBounds(735, 260, 245, 150);
-		sellPanel.setVisible(false);
-		add(sellPanel);
-		sellPanel.setLayout(null);
-		
-		JPanel sellLabelPanel = new JPanel();
-		sellLabelPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		sellLabelPanel.setBounds(6, 6, 233, 41);
-		sellPanel.add(sellLabelPanel);
-		sellLabelPanel.setLayout(null);
-		
-		JLabel sellLabel = new JLabel("Sell Items");
-		sellLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
-		sellLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		sellLabel.setBounds(6, 6, 221, 29);
-		sellLabelPanel.add(sellLabel);
-		
-		inventoryComboBox = new JComboBox<Item>();
-		inventoryComboBox.setBounds(6, 59, 233, 27);
-		for(Item el : hero.getInventory())
-			if(!el.isEquipped()){
-				inventoryComboBox.addItem(el);
-			}
-		sellPanel.add(inventoryComboBox);
-		
-		JButton sellBtn = new JButton("Sell");
-		sellBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Item i = (Item)inventoryComboBox.getSelectedItem();
-				hero.addGold(i.getItemGold());
-				hero.removeFromInventory((Item)inventoryComboBox.getSelectedItem());
-				inventoryComboBox.removeItem(inventoryComboBox.getSelectedItem());
-				reloadInventoryPanel(false);
-			}
-		});
-		sellBtn.setBounds(56, 98, 117, 35);
-		sellPanel.add(sellBtn);
-		
-		enhancePanel = new JPanel();
-		enhancePanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		enhancePanel.setBounds(735, 422, 245, 150);
-		enhancePanel.setVisible(false);
-		add(enhancePanel);
-		enhancePanel.setLayout(null);
-		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null), new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
-		panel_4.setBounds(6, 6, 233, 37);
-		enhancePanel.add(panel_4);
-		panel_4.setLayout(null);
-		
-		JLabel lblNewLabel_2 = new JLabel("Improve Items");
-		lblNewLabel_2.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 20));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(6, 6, 221, 25);
-		panel_4.add(lblNewLabel_2);
-		
-		enhanceComboBox = new JComboBox();
-		enhanceComboBox.setBounds(6, 55, 233, 27);
-		for(Item el : hero.getInventory())
-			enhanceComboBox.addItem(el);
-		enhancePanel.add(enhanceComboBox);
-		
-		JButton enhancheBtn = new JButton("Improve");
-		enhancheBtn.setBounds(56, 98, 117, 35);
-		enhancePanel.add(enhancheBtn);
+		JLabel lblNewLabel = new JLabel("Items for Sale");
+		lblNewLabel.setBackground(Color.LIGHT_GRAY);
+		lblNewLabel.setBounds(0, 0, 456, 39);
+		salePanel.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 23));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 			
 
 			this.renderBackground();
@@ -371,6 +350,9 @@ public class Blacksmith extends Area {
 	public void loadImages() {
 		try {           
 			this.background = new ImageIcon(ImageIO.read(new File("insideBlacksmith.jpg")));
+			this.buyButtonIcon = new ImageIcon(ImageIO.read(new File("attack.png")));
+			this.sellButtonIcon = new ImageIcon(ImageIO.read(new File("sellcoins.png")));
+			this.enhanceButtonIcon = new ImageIcon(ImageIO.read(new File("anvil.png")));
 			
 		} catch (IOException e) {
 			e.printStackTrace();
